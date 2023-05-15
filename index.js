f8facb34b2d5071e9934cf1a392e1733931b6ed8
Vishal Joshi <vishal.joshi1@gmail.com>
2023-05-15 13:13:17 +0530

    assignment-2

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
-
 document.querySelector(".search-bar button").addEventListener("click",function(){
-  document.querySelector(".search-bar input").classList.toggle("show")
-  console.log(document.querySelector(".search-bar input").classList);
-  if(document.querySelector(".search-bar input").classList[0] == "show"){
-    document.querySelector(".search-bar img").setAttribute('src', "media/cross.png")
-  }
-  else{
-    document.querySelector(".search-bar img").setAttribute('src', "media/lens.png")
-  }
-})
+    document.querySelector(".search-bar input").classList.toggle("show")
+    if(document.querySelector(".search-bar input").classList[0] == "show"){
+      document.querySelector(".search-bar img").setAttribute('src', "media/cross.png")
+    }
+    else{
+      document.querySelector(".search-bar img").setAttribute('src', "media/lens.png")
+    }
+  })
+  
